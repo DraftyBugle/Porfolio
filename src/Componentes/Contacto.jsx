8058f81090a2cef191564ded67a08afa4d35cb9c
Federico <fedeterrezas@gmail.com>
2023-12-13 17:12:16 -0300

    New

--- src/Componentes/Contacto.jsx
+++ src/Componentes/Contacto.jsx
@@ -13,7 +13,7 @@ function Contacto (){
                 <li > <img src={linkedinIcon} alt="" className="Icon1" />
                 <a href="https://www.linkedin.com/in/federico-terrazas-05325027b/" className="Link">FedericoMagunaTerrazas</a></li>
                 <li > <img src={Email} alt="" className="Icon2" />  <a href="magunat.m.federico@gmail.com"   className="Email">Magunat.m.federico@gmail.com</a></li>
-                <li > <img src={Wpp} alt="" className="Icon3" />      <a href="wa.link/b4m1wu" className="Wpp">+54 1150094804</a></li>
+                <li > <img src={Wpp} alt="" className="Icon3" />      <a href="wa.link/b4m1wu" className="Wpp">+54 9 11 2397-3817</a></li>
             </ul>
              
             </div>
